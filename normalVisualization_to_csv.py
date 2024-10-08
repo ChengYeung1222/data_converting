019e202dd2086ecd8de610a301a8a799cef9dfb6
ChengYeung1222 <35996060+ChengYeung1222@users.noreply.github.com>
2024-10-08 10:58:11 +0200

    add reflectance

--- normalVisualization_to_csv.py
+++ normalVisualization_to_csv.py
@@ -3,15 +3,15 @@ import h5py
 import pandas as pd
 import logging
 
-
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
-                    filename='visualization_nan.log',
+                    filename='visualization.log',
                     filemode='w')  # Use 'a' for append mode
 
 INCLUDE_CURVATURE = True
 INCLUDE_FLATNESS = False
 INCLUDE_DENSITY = False
 INCLUDE_INTENSITY = True
+INCLUDE_REFLECTANCE = True
 
 
 def merge_tiles_to_csv(h5_file_path, csv_file_path):
@@ -57,7 +57,7 @@ def merge_tiles_to_txt_normals(h5_file_path, txt_file_path):
 
 
 def merge_tiles_to_txt(h5_file_path, txt_file_path, include_curvature=False, include_flatness=False,
-                       include_density=False,include_intensity=False):
+                       include_density=False, include_intensity=False, include_reflectance=False):
     """
     Merge tiles and save them to a text file for visualization, with optional inclusion of curvature, flatness, and density.
 
@@ -87,10 +87,15 @@ def merge_tiles_to_txt(h5_file_path, txt_file_path, include_curvature=False, inc
         num_features += 1  # Add density
     if include_intensity:
         num_features += 1  # Add intensity
+    if include_reflectance:
+        num_features += 1
 
     # Reshape the data based on the selected features
-    data = data.reshape(-1, num_features)  # Flatten the data array to (N, num_features)
-    labels = labels.reshape(-1, 1)  # Flatten the label array to (N, 1)
+    if 'ts1' in txt_file_path:
+        pass
+    else:
+        data = data.reshape(-1, num_features)  # Flatten the data array to (N, num_features)
+        labels = labels.reshape(-1, 1)  # Flatten the label array to (N, 1)
 
     if np.isnan(data).any():
         logging.warning("NaN values found.")
@@ -112,7 +117,11 @@ def merge_tiles_to_txt(h5_file_path, txt_file_path, include_curvature=False, inc
     #         logging.info(f"Filtered NaN values from intensity. New data shape: {data.shape}")
 
     # Combine data and labels into a single array
-    combined_data = np.hstack((data, labels))  # Combine data and labels
+    if 'ts1' in txt_file_path:
+        data_squeezed = np.squeeze(data, axis=1)  # 去掉第二个维度，结果形状为 (4828486, 9)
+        combined_data = np.hstack((data_squeezed, labels))  # 合并数据和标签
+    else:
+        combined_data = np.hstack((data, labels))  # Combine data and labels
 
     # Write to text file
     with open(txt_file_path, 'w') as txt_file:
@@ -126,6 +135,8 @@ def merge_tiles_to_txt(h5_file_path, txt_file_path, include_curvature=False, inc
             header += ' density'
         if include_intensity:
             header += ' intensity'
+        if include_reflectance:
+            header += ' reflectance'
         header += ' classification\n'
         txt_file.write(header)
 
@@ -182,20 +193,25 @@ def merge_tiles_to_csv_expired(h5_file_path, csv_file_path, relevant_tiles):
 # txt_file_path = './scene_YellowArea_2cm_val_2048_fps_05m_normal_curvature.txt'
 # h5_file_path = './scene_open_space_merged_2cm_train_2048_fps_2m_normal_curvature_intensity_buffer_wofilter.h5'
 # txt_file_path = './scene_open_space_merged_2cm_train_2048_fps_2m_normal_curvature_intensity_buffer_wofilter.txt'
-h5_file_path = './scene_RedArea_2cm_train_2048_fps_2m_normal_curvature_intensity_buffer_withfilter_reflectance.h5'
-txt_file_path = './scene_RedArea_2cm_train_2048_fps_2m_normal_curvature_intensity_buffer_withfilter_reflectance.txt'
+# h5_file_path = './scene_open_space_2cm_train_2048_fps_2m_normal_curvature_intensity_buffer_wofilter_reflectance_refined.h5'
+# txt_file_path = './scene_open_space_2cm_train_2048_fps_2m_normal_curvature_intensity_buffer_wofilter_reflectance_refined.txt'
 # h5_file_path = './scene_YellowArea_2cm_val_2048_fps_05m_normal_curvature_flatness_41tiles.h5'
 # txt_file_path = './scene_YellowArea_2cm_val_2048_fps_05m_normal_curvature_flatness_41tiles.txt'
 # h5_file_path = './scene_RedArea_2cm_train_2048_fps_2m_normal_curvature.h5'
 # txt_file_path = './scene_RedArea_2cm_train_2048_fps_2m_normal_curvature.txt'
 # h5_file_path = './scene_RedArea_2cm_train_2048_fps_2m_normal_curvature_intensity.h5'
 # txt_file_path = './scene_RedArea_2cm_train_2048_fps_2m_normal_curvature_intensity.txt'
+# h5_file_path = './scene_RedArea_2cm_train_2048_fps_2m_normal_curvature_intensity_buffer_withfilter_reflectance.h5'
+# txt_file_path = './scene_RedArea_2cm_train_2048_fps_2m_normal_curvature_intensity_buffer_withfilter_reflectance.txt'
+h5_file_path = './scene_open_space_2cm_train_2048_fps_2m_normal_curvature_intensity_buffer_wofilter_reflectance_refined_ts1.h5'
+txt_file_path = './scene_open_space_2cm_train_2048_fps_2m_normal_curvature_intensity_buffer_wofilter_reflectance_refined_ts1.txt'
 
 # relevant_tiles = [(i, k) for i in range(1) for k in range(len(x_lim))]
 
 # merge_tiles_to_csv(h5_file_path, csv_file_path)
 merge_tiles_to_txt(h5_file_path, txt_file_path, include_curvature=INCLUDE_CURVATURE, include_flatness=INCLUDE_FLATNESS,
-                   include_density=INCLUDE_DENSITY,include_intensity=INCLUDE_INTENSITY)
+                   include_density=INCLUDE_DENSITY, include_intensity=INCLUDE_INTENSITY,
+                   include_reflectance=INCLUDE_REFLECTANCE)
 
 # print(f'Saved merged tiles to {csv_file_path}')
 print(f'Saved merged tiles to {txt_file_path}')
